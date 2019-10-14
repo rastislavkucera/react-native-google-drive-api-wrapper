1cf726fe8bd5b2b3a3e89ee108724f367760fd2e
Rastislav Kučera <44438265+rastislavkucera@users.noreply.github.com>
2019-10-14 11:46:42 +0200

    Add accessToken to TS

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ export function init(): void;
 export function setAccessToken(token: string): void; 
 export function isInitialized(): boolean;
 
+export const accessToken: string;
+
 export const files: {
     createFileMultipart: (media: string, mediaType: string, metadata: any, isBase64: boolean) => Promise<GlobalFetch>,
     download: (fileID: string, params: any) => Promise<GlobalFetch>,
